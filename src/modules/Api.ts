f7c828aa1f150aeee847997048258bb4a3fbfb31
Mat Jones <mjones44@ycp.edu>
2019-06-16 16:43:39 -0400

    force heroku rebuild

--- src/modules/Api.ts
+++ src/modules/Api.ts
@@ -1,15 +1,18 @@
 import Brewery from './Brewery';
 import Filter from './Filter';
 const axios = require('axios');
+
 const url = function (endpoint: string): string {
     return `https://api.openbrewerydb.org/${endpoint}`;
 };
+
 const getMultiple = function(endpoint: string): Promise<Array<Brewery>> {
     return new Promise( (resolve: Function, reject: Function) => {
         let promise = axios.get(url(endpoint)).catch((e: Error) => console.error(e || `Uknown error getting ${url(endpoint)}`));
         promise.then(response => resolve(response.data)).catch(e => reject(e));
     })
 };
+
 const get = function(endpoint: string): Promise<Brewery> {
     return new Promise( (resolve: Function, reject: Function) => {
         let promise = axios.get(url(endpoint)).catch((e: Error) => console.error(e || `Uknown error getting ${url(endpoint)}`));

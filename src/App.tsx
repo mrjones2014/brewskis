195da4c4f589071946e754569a1ab235a4828f12
Mat Jones <mjones44@ycp.edu>
2019-06-16 16:31:45 -0400

    move background img

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import DataPanel from './view/DataPanel';
 import DetailView from './view/DetailView';
 import { Route, Switch } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
-import beer from './beer.jpg';
-
 require('./App.scss');
 
 const App: React.FC = () => {
